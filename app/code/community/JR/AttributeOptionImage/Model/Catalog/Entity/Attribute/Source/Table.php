9f57c8765f17773ffc54f274ef962588f2a462bb
Artus Kolanowski <artus@beluto.com>
2012-07-09 09:30:28 +0200

    Fetch images and thumbnails by value
    Provides methods to fetch images and thumbnails of attribute options by 
    value(s) like it Magento does for their text labels.

--- app/code/community/JR/AttributeOptionImage/Model/Catalog/Entity/Attribute/Source/Table.php
+++ app/code/community/JR/AttributeOptionImage/Model/Catalog/Entity/Attribute/Source/Table.php
@@ -39,7 +39,61 @@ class JR_AttributeOptionImage_Model_Catalog_Entity_Attribute_Source_Table extend
         if ($withEmpty) {
             array_unshift($options, array('label' => '', 'value' => '', 'image' => null, 'thumbnail' => null)); // [patch]
         }
-
+    
         return $options;
+    }
+    
+    /**
+     * Get a image for option value
+     *
+     * @param string|integer $value
+     * @return string|array
+     */
+    public function getOptionImage($value) {
+        return $this->_getOptionExtension($value, 'image');
+    }
+    
+    /**
+     * Get a thumbnail for option value
+     *
+     * @param string|integer $value
+     * @return string|array
+     */
+    public function getOptionThumbnail($value) {
+        return $this->_getOptionExtension($value, 'thumbnail');
+    }
+    
+    /**
+     * Get a extended field for option value
+     *
+     * @param string|integer $value
+     * @param string $field
+     * @return string|array
+     */
+    protected function _getOptionExtension($value, $field) {
+        $isMultiple = false;
+        if (strpos($value, ',')) {
+            $isMultiple = true;
+            $value = explode(',', $value);
+        }
+    
+        $options = $this->getAllOptions(false);
+    
+        if ($isMultiple) {
+            $values = array();
+            foreach ($options as $item) {
+                if (in_array($item['value'], $value)) {
+                    $values[] = $item[$field];
+                }
+            }
+            return $values;
+        }
+    
+        foreach ($options as $item) {
+            if ($item['value'] == $value) {
+                return $item[$field];
+            }
+        }
+        return false;
     }
 }
